13f888db6bd633a572965f2885ad9499621bb14e
Dinesh Panda <dineshpanda92@gmail.com>
2020-05-26 00:43:00 +0530

    Store current user id in browser cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_user
 
   
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def set_user
+    cookies[:current_user_id] = current_user&.id
+  end
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :avatar, :first_name, :last_name, :bio])
