ef934f65e54b439f8f38bfc536896d7b540129f4
Dinesh Panda <dineshpanda92@gmail.com>
2020-05-29 15:19:22 +0530

    Add request header logging

--- app/controllers/api/v1/graphiti_controller.rb
+++ app/controllers/api/v1/graphiti_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::GraphitiController < ApplicationController
   skip_before_action :authenticate_user!
   before_action :doorkeeper_authorize!
+  before_action :log_access_token
 
   include Graphiti::Rails::Responders
 
+  private
+
+  def log_access_token
+    Rails.logger.info "\n=================== Access Token ======================\n"
+    Rails.logger.info request.headers["authorization"]
+    Rails.logger.info "\n=================== Access Token ======================\n"
+  end
 end
